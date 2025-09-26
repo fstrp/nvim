8b034cd2eeeccac73afdcf3c9d7e22bc9cf1a625
fstrp <fabiostra@hotmail.com>
2025-09-26 22:29:28 +0200

    add easy-dotnet

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -1,46 +1,53 @@
 ---@module 'lazy.core.plugin'
 ---@type LazySpec
 return {
-	"saghen/blink.cmp",
-	dependencies = "rafamadriz/friendly-snippets",
-	version = "*",
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
-	opts = {
-		keymap = {
-			preset = "default",
-			["<tab>"] = { "select_and_accept", "fallback" },
-			["<c-l>"] = { "snippet_forward" },
-			["<c-h>"] = { "snippet_backward" },
-		},
-		cmdline = {
-			keymap = { preset = "inherit" },
-			completion = { menu = { auto_show = true } },
-		},
-		appearance = { nerd_font_variant = "normal" },
-		sources = {
-			default = { "lsp", "path", "snippets" },
-			providers = {
-				snippets = {
-					score_offset = 2,
-					min_keyword_length = 3,
-				},
-				path = { score_offset = 1 },
-			},
-		},
-		completion = {
-			accept = { create_undo_point = false },
-			ghost_text = { enabled = true },
-			documentation = { auto_show = true },
-			list = {
-				selection = {
-					preselect = false,
-					auto_insert = false,
-				},
-			},
-		},
-		signature = { enabled = true },
-		fuzzy = { use_frecency = false },
-	},
-	opts_extend = { "sources.default" },
+    "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "*",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        keymap = {
+            preset = "default",
+            ["<tab>"] = { "select_and_accept", "fallback" },
+            ["<c-l>"] = { "snippet_forward" },
+            ["<c-h>"] = { "snippet_backward" },
+        },
+        cmdline = {
+            keymap = { preset = "inherit" },
+            completion = { menu = { auto_show = true } },
+        },
+        appearance = { nerd_font_variant = "normal" },
+        sources = {
+            default = { "lsp", "easy-dotnet", "path", "snippets" },
+            providers = {
+                snippets = {
+                    score_offset = 2,
+                    min_keyword_length = 3,
+                },
+                path = { score_offset = 1 },
+                ["easy-dotnet"] = {
+                    name = "easy-dotnet",
+                    enabled = true,
+                    module = "easy-dotnet.completion.blink",
+                    score_offset = 10000,
+                    async = true,
+                },
+            },
+        },
+        completion = {
+            accept = { create_undo_point = false },
+            ghost_text = { enabled = true },
+            documentation = { auto_show = true },
+            list = {
+                selection = {
+                    preselect = false,
+                    auto_insert = false,
+                },
+            },
+        },
+        signature = { enabled = true },
+        fuzzy = { frecency = { enabled = false } },
+    },
+    opts_extend = { "sources.default" },
 }
---
+++ lua/plugins/easy-dotnet.lua
@@ -0,0 +1,7 @@
+---@module 'lazy.core.plugin'
+---@type LazySpec
+return {
+    "GustavEikaas/easy-dotnet.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    opts = {},
+}
