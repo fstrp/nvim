ef44d7112a30098d915fc42111f1c2e36accdd40
fstrp <fabiostra@hotmail.com>
2025-09-26 22:29:27 +0200

    Add nvim-ufo

--- lua/opt.lua
+++ lua/opt.lua
@@ -55,11 +55,3 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
-if vim.g.fast == true then
-	return
-end
-
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldlevelstart = 99
---
+++ lua/plugins/ufo.lua
@@ -0,0 +1,14 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	config = function()
+		vim.opt.foldlevel = 99
+		vim.opt.foldlevelstart = 99
+
+		require("ufo").setup({
+			provider_selector = function()
+				return { "treesitter", "indent" }
+			end,
+		})
+	end,
+}
